f39e532fb48dd5df142e7f8bcc8dc690531c4fa5
Ivan Dimitrov <biobeehive@abv.bg>
2022-09-22 06:20:48 +0300

    start

--- Pr_GH+BT/ContentView.swift
+++ Pr_GH+BT/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.yellow.opacity(0.3).ignoresSafeArea()
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
