66e75070056ab20826f54089e6b6bc54091e6c3c
Ivan Dimitrov <biobeehive@abv.bg>
2022-09-22 05:50:31 +0300

    Initial Commit

---
+++ Pr_GH+BT/Pr_GH_BTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pr_GH_BTApp.swift
+//  Pr_GH+BT
+//
+//  Created by Dosi Dimitrov on 22.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Pr_GH_BTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pr_GH+BTUITests/Pr_GH_BTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pr_GH_BTUITestsLaunchTests.swift
+//  Pr_GH+BTUITests
+//
+//  Created by Dosi Dimitrov on 22.09.22.
+//
+
+import XCTest
+
+class Pr_GH_BTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
