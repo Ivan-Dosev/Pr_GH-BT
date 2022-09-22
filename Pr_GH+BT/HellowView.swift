67e6bd37c440f059cc3b352d1486ca9a8b60e518
Ivan Dimitrov <biobeehive@abv.bg>
2022-09-22 10:25:53 +0300

    hello

---
+++ Pr_GH+BT/HellowView.swift
@@ -0,0 +1,22 @@
+//
+//  HellowView.swift
+//  Pr_GH+BT
+//
+//  Created by Dosi Dimitrov on 22.09.22.
+//
+
+import SwiftUI
+
+struct HellowView: View {
+    var body: some View {
+        ZStack {
+            Text("HellowView")
+        }
+    }
+}
+
+struct HellowView_Previews: PreviewProvider {
+    static var previews: some View {
+        HellowView()
+    }
+}
