137f820c5a788ffa48a62be468dac7d91f03dfbc
Ivan Dimitrov <biobeehive@abv.bg>
2022-09-22 07:01:43 +0300

    picker view

---
+++ Pr_GH+BT/PickerView.swift
@@ -0,0 +1,25 @@
+//
+//  PickerView.swift
+//  Pr_GH+BT
+//
+//  Created by Dosi Dimitrov on 22.09.22.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        ZStack {
+            HStack {
+                
+                Text("PickerView")
+            }
+        }
+    }
+}
+
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
