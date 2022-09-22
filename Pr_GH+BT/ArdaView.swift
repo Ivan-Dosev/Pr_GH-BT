dee031d1c0d277f22beadd2a54d426d4c4fbb86f
Ivan Dimitrov <biobeehive@abv.bg>
2022-09-22 06:57:51 +0300

    arda view

---
+++ Pr_GH+BT/ArdaView.swift
@@ -0,0 +1,25 @@
+//
+//  ArdaView.swift
+//  Pr_GH+BT
+//
+//  Created by Dosi Dimitrov on 22.09.22.
+//
+
+import SwiftUI
+
+struct ArdaView: View {
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            HStack {
+                Text(" ArdaView ")
+            }
+        }
+    }
+}
+
+struct ArdaView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArdaView()
+    }
+}
